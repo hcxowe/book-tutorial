25a084f0fa127e984281ea9d88bf853093701891
hcxowe <hcxowe@126.com>
2016-12-18 16:51:21 +0800

    中介者模式学习

---
+++ JSDesignPatterns/tutorial-01/tutorial.6.js
@@ -0,0 +1,76 @@
+// ### Mediator 中介者模式
+// 中介者是一种行为设计模式，它为我们公开一个统一的接口，系统的不同部分可通过这个接口进行通信
+
+// 简单的中介模式
+var mediator = (function() {
+
+    var topics = {};
+
+    var subscribe = function(topic, fn) {
+
+        if (!topics[topic]) {
+            topics[topic] = [];
+        }
+
+        topics[topic].push({
+            context: this,
+            callback: fn
+        });
+
+        return this;
+    };
+
+    var publish = function(topic) {
+
+        if (!topics[topic]) {
+            return false;
+        }
+
+        var args = Array.prototype.slice.call(arguments, 1);
+
+        for (var i = 0, len = topics[topic].length; i < len; i++) {
+            var subscription = topics[topic][i];
+            subscription.callback.apply(subscription.context, args);
+        }
+
+        return this;
+    };
+
+    return {
+        Publish: publish,
+        Subscribe: subscribe,
+        installTo: function(obj) {
+            obj.subscribe = subscribe;
+            obj.publish = publish;
+        }
+    };
+
+}());
+
+// eg
+$('#chatForm').on('submit', function(e) {
+    e.preventDefault();
+
+    var text = $('#chatBox').val(),
+        from = $('#fromBox').val(),
+        to = $('#toBox').val();
+
+    mediator.Publish('newMessage', { message: text, from: from, to: to });
+
+    return false;
+});
+
+mediator.Subscribe('newMessage', function(data) {
+    var date = new Date(),
+        msg = data.from + ' said "' + data.message + ' "to" ' + data.to;
+
+    $('#chatResult').prepend("" + msg + " (" + date.toLocaleTimeString() + ")");
+});
+
+mediator.Subscribe('newMessage', function(data) {
+    console.log(data);
+});
+
+// ### 如何理解中介者与观察者之间的区别？
+// 观察者模式中，不存在封装约束的单一对象，Subject与Observer必须合作才能维持约束
+//
