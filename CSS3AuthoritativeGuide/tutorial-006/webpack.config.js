0039ee4bfa24b5bf05cb9a97c0f9b44fcc8c69c9
hcxowe <hcxowe@126.com>
2017-04-25 15:48:36 +0800

    渐变

---
+++ CSS3AuthoritativeGuide/tutorial-006/webpack.config.js
@@ -0,0 +1,57 @@
+var path = require('path');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+// 多个提取实例
+const extractCSS = new ExtractTextPlugin('custom.css');
+const extractLESS = new ExtractTextPlugin('common.css');
+
+module.exports = {
+    context: path.resolve(__dirname, 'app'),
+    entry: {
+        main: './main.js'
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.less$/,
+                use: extractLESS.extract({
+                    fallback: "style-loader",
+                    use: ["css-loader", 'postcss-loader', 'less-loader']
+                })
+            },
+            {
+                test: /\.css$/,
+                use: extractCSS.extract({
+                    fallback: "style-loader",
+                    use: ["css-loader"]
+                })
+            },
+            {
+                test: /\.(jpg|png)$/,
+                use: 'url-loader?limit=4096'
+            }
+        ]
+    },
+
+    devServer: {
+        inline: true,
+        port: 9000,
+        open: true
+    },
+
+    plugins: [
+        extractCSS,
+
+        extractLESS,
+
+        new HtmlWebpackPlugin({
+            filename: './index.html',
+            template: './template/index.html'
+        })
+    ]
+}
