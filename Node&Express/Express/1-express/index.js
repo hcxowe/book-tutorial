54310d702f10bd1ba87a7142862066f3ef9251ec
hcxowe <hcxowe@126.com>
2017-07-27 18:44:05 +0800

    handlebars模板，静态目录，搭建一个简单的Express

---
+++ Node&Express/Express/1-express/index.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var exphbs  = require('express-handlebars');
+
+var app = express();
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+//app.engine('handlebars', exphbs.create({ defaultLayout: 'main' }).engine);
+app.set('view engine', 'handlebars');
+
+//app.set('views', __dirname + '/views');
+
+app.set('port', process.env.PORT || 3000);
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+
+app.use(function(req, res) {
+    res.status(404);
+    res.render('404');
+});
+
+app.listen(app.get('port'), function() {
+    console.log('Express started on http://localhost:' + app.get('port'));
+});
